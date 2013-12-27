2be04e7b1f3824d5e20a49cefa1f978f61a7342d
Matt Barry <mrb@softisms.com>
2013-12-27 12:21:47 -0600

    Assignment consistency

--- pbo.R
+++ pbo.R
@@ -15,18 +15,18 @@ require(PerformanceAnalytics,quietly=TRUE) # for performance measure
 
 ##### profile
 
-INF_SUB = 6             # replace Inf with something for plotting
-BAR_COLOR = "#cc99cc"   # plot bar fill color
-LINE_COLOR = "#3366cc"  # plot line color
-TEST_COLOR = "#339999"  # plot test line color
+INF_SUB <- 6             # replace Inf with something for plotting
+BAR_COLOR <- "#cc99cc"   # plot bar fill color
+LINE_COLOR <- "#3366cc"  # plot line color
+TEST_COLOR <- "#339999"  # plot test line color
 
 ##### test setup t100
 # populating the matrix M with samples from an installed distribution
 # see also some reference tests in the tests file
 
-N = 200                 # studies, alternative configurations
-T = 3200                # sample returns
-S = 8                   # partition count
+N <- 200                 # studies, alternative configurations
+T <- 3200                # sample returns
+S <- 8                   # partition count
 
 M <- data.frame(matrix(NA,T,N,byrow=TRUE,dimnames=list(1:T,1:N)),
                 check.names=FALSE)
@@ -35,92 +35,92 @@ for ( i in 1:N ) M[,i] <- rt(T,10) / 100
 
 ##### derived setup
 
-CS = combn(S,S/2)       # combinations
-SN = T / S              # partition size
-test_config = bquote(N == .(N) ~~ T == .(T) ~~ S == .(S))
+CS <- combn(S,S/2)       # combinations
+SN <- T / S              # partition size
+test_config <- bquote(N == .(N) ~~ T == .(T) ~~ S == .(S))
 
 ##### analyze
 
 
 # initialize results lists
-cs_results = list()
+cs_results <- list()
 
 # for each partition combination
 for ( cs in 1:ncol(CS) ) {
   
   # partition indices
-  IS_I = CS[,cs]
+  IS_I <- CS[,cs]
   
   # in-sample indices
-  is_indices = as.vector(sapply(IS_I,function(i) {
-    start = SN * i - SN + 1
-    end = start + SN - 1
+  is_indices <- as.vector(sapply(IS_I,function(i) {
+    start <- SN * i - SN + 1
+    end <- start + SN - 1
     start:end
   }))
   
   # out-of-sample indices
-  os_indices = which(1:T != is_indices)
+  os_indices <- which(1:T != is_indices)
   
   # training and test sets (in sample, out of sample)
   # choosing not to reassign row names of each to 1:(T/2)
   # after R don't need to save J or J_bar so could skip this assignment
-  J = M[is_indices,]
-  J_bar = M[os_indices,]
+  J <- M[is_indices,]
+  J_bar <- M[os_indices,]
   
   # compute Omega ratio over the N strategies in each subset
   # could use for R any summary statistic e.g. SharpeRatio
-  R = mapply(Omega,J) # mapply(sharpe,J) 
-  R_bar = mapply(Omega,J_bar) # mapply(sharpe,J_bar)
+  R <- mapply(Omega,J) # mapply(sharpe,J) 
+  R_bar <- mapply(Omega,J_bar) # mapply(sharpe,J_bar)
   
   # compute n* by argmax over R vector
-  n_star = which.max(R)
+  n_star <- which.max(R)
   
   # rank of n*th result from OOS performance; converted to (0,1) interval
-  omega_bar_c = rank(R_bar)[n_star] / length(R_bar)
+  omega_bar_c <- rank(R_bar)[n_star] / length(R_bar)
   
   # logit
   # note the value can be Inf
-  lambda_c = log(omega_bar_c / (1 - omega_bar_c))
+  lambda_c <- log(omega_bar_c / (1 - omega_bar_c))
   
   # save the results
-  cs_results = rbind(cs_results,list(R,R_bar,n_star,omega_bar_c,lambda_c))
+  cs_results <- rbind(cs_results,list(R,R_bar,n_star,omega_bar_c,lambda_c))
 }
 
 colnames(cs_results) <- c("R","R_bar","n*","omega_bar","lambda")
 rownames(cs_results) <- 1:ncol(CS)
 
-lambda = as.numeric(cs_results[,"lambda"])
-lambda[which(lambda==Inf)] = INF_SUB
+lambda <- as.numeric(cs_results[,"lambda"])
+lambda[which(lambda==Inf)] <- INF_SUB
 
 # probability of backtest overfit
 # using MC test count approach of lambda count
-phi = sum(ifelse(lambda<=0,1,0))/ncol(CS)
+phi <- sum(ifelse(lambda<=0,1,0))/ncol(CS)
 # -- alternative might use relative frequency sum 
-# rf = as.data.frame( table(lambda) / ncol(CS), stringAsFactors=FALSE)
-# phi = sum(ifelse(rf$lambda <= 0, rf$Freq, 0))
+# rf <- as.data.frame( table(lambda) / ncol(CS), stringAsFactors=FALSE)
+# phi <- sum(ifelse(rf$lambda <= 0, rf$Freq, 0))
 # -- alternative might use density fit
-# d = density(lambda,kernel="rectangular")
+# d <- density(lambda,kernel="rectangular")
 
 # performance degradation
-rn_pairs = as.data.frame(do.call(rbind,lapply(1:ncol(CS),function(i) {
-  n = cs_results[[i,3]]
-  r = cs_results[[i,1]]
-  rb = cs_results[[i,2]]
+rn_pairs <- as.data.frame(do.call(rbind,lapply(1:ncol(CS),function(i) {
+  n <- cs_results[[i,3]]
+  r <- cs_results[[i,1]]
+  rb <- cs_results[[i,2]]
   return(c(r[n],rb[n]))
 })))
 colnames(rn_pairs) <- c("Rn","Rbn")
 
 # linear fit to pairs, extract results for plot annotations
-linear_fit = lm(rn_pairs)
-m = signif(as.numeric(linear_fit$coefficients[1]),digits=5) # slope
-b = signif(as.numeric(linear_fit$coefficients[2]),digits=5) # intercept
-ar2 = signif(summary(linear_fit)$adj.r.squared,digits=2) # adj R-squared
+linear_fit <- lm(rn_pairs)
+m <- signif(as.numeric(linear_fit$coefficients[1]),digits=5) # slope
+b <- signif(as.numeric(linear_fit$coefficients[2]),digits=5) # intercept
+ar2 <- signif(summary(linear_fit)$adj.r.squared,digits=2) # adj R-squared
   
 # probability R(OOS) < 1 for Omega, ratio of <1 to total
 # we're using Omega so threshold 1 rather than 0 for Sharpe
-threshold = 1
+threshold <- 1
 
-p_oos_lt1 = signif(length(which(rn_pairs$Rbn<threshold)) / 
+p_oos_lt1 <- signif(length(which(rn_pairs$Rbn<threshold)) / 
                      nrow(rn_pairs),digits=3)
 
 #################### GRAPHICS
@@ -162,7 +162,7 @@ histogram(lambda,
 # performance degradation and probability of loss
 
 # plot Rn pairs
-cloud_span = c(signif(min(rn_pairs),-3),
+cloud_span <- c(signif(min(rn_pairs),-3),
                signif(max(rn_pairs),3)) # axis range
 
 xyplot(rn_pairs$Rbn ~ rn_pairs$Rn,
@@ -206,11 +206,11 @@ xyplot(rn_pairs$Rbn ~ rn_pairs$Rn,
 # stochastic dominance
 # uses n* items from R-bar for one line, and all n items from R-bar for the other line
 # create cumulative distribution functions for each data set, then generate samples to plot
-y = seq(min(rn_pairs$Rbn),max(rn_pairs$Rbn),0.01) # reasonable R range for evaluation
-erbn = ecdf(rn_pairs$Rbn) # optimized
-erb = ecdf(sapply(1:ncol(CS),function(i) cs_results[[i,2]])) # non-optimized (all)
-sorted = data.frame(cbind(sort(erbn(y)),sort(erb(y))))
-sorted$sd2 = sorted$X2 - sorted$X1 
+y <- seq(min(rn_pairs$Rbn),max(rn_pairs$Rbn),0.01) # reasonable R range for evaluation
+erbn <- ecdf(rn_pairs$Rbn) # optimized
+erb <- ecdf(sapply(1:ncol(CS),function(i) cs_results[[i,2]])) # non-optimized (all)
+sorted <- data.frame(cbind(sort(erbn(y)),sort(erb(y))))
+sorted$sd2 <- sorted$X2 - sorted$X1 
 colnames(sorted) <- c("Rbn","Rb","SD2")
 
 xyplot(Rbn + Rb ~ y, 
